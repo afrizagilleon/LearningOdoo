0183bd4a1126518b592a0d4b7a994b41e4411f3c
afrizagilleon <afriza.gilleon@gmail.com>
2024-06-26 19:36:51 +0700

    Feat: Create the basic module (application) to Real Estate Ads

---
+++ custom-addons/real_estate_ads/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Real Estate Ads",
+    "version": "1.0",
+    "author": "Afriza Gilleon",
+    "maintainer": "Afriza Gilleon",
+
+    "description" : """
+        Real Estate module to show available properties
+    """,
+    "category" : "Sales",
+    "depends" : [],
+    "data" : [],
+    "installable": True,
+    "application": True,
+    "license": "AGPL-3",
+}
