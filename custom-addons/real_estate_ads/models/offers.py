b071f05608efac6be149688ccf6a833aeec5f016
afrizagilleon <afriza.gilleon@gmail.com>
2024-07-01 15:10:32 +0700

    feat: 1. create computed deadline field 2. create onchange on `creation_date` or `deadline` to update `validity` field

--- custom-addons/real_estate_ads/models/offers.py
+++ custom-addons/real_estate_ads/models/offers.py
@@ -1,4 +1,5 @@
-from odoo import fields, models
+from odoo import fields, models, api
+from datetime import timedelta
 
 
 class Offer(models.Model):
@@ -11,8 +12,21 @@ class Offer(models.Model):
         string="Status")
 
     validity = fields.Integer(string="Validity")
-    deadline = fields.Datetime(string="Deadline")
+    deadline = fields.Date(
+        string="Deadline",
+        compute="_compute_deadline", inverse=True)
+    creation_date = fields.Date(string="Creation Date", default=fields.Datetime.now)
 
     # Relations
     partner_id = fields.Many2one('res.partner', string="Partner")
     property_id = fields.Many2one('estate.property', string="Property")
+
+    @api.depends('validity', 'creation_date')
+    def _compute_deadline(self):
+        for rec in self:
+            rec.deadline = rec.creation_date + timedelta(days=rec.validity)
+
+    @api.onchange('creation_date', 'deadline')
+    def _onchange_deadline(self):
+        for rec in self:
+            rec.validity = (rec.deadline - rec.creation_date).days
